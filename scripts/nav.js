860da483f91e748616229c266f65cc2449585992
Soleil A Phan <soleilphan@gmail.com>
2015-11-11 11:29:19 -0800

    v3 first

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+var num = 30; //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.nav').addClass('navscroll');
+    } else {
+        $('.nav').removeClass('navscroll');
+    }
+});
